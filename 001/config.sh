865fd9d50cafd5396d6a80282b522d1a061f925f
wsslb1 <cl3223@ic.ac.uk>
2024-05-22 17:34:11 +0100

    Add files via upload

---
+++ 001/config.sh
@@ -0,0 +1,15 @@
+export DA_EXPDIR=~/experiments
+ 
+export DA_WORKDIR=$EPHEMERAL
+ 
+export DA_TOOLSDIR=~/u-dales/tools
+ 
+export DA_BUILD=~/u-dales/build/release/u-dales
+ 
+export NCPU=16
+ 
+export NNODE=1
+ 
+export WALLTIME="01:00:00"
+ 
+export MEM="512gb"
